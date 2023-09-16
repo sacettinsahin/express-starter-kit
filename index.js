60fac588848c260e62eb76ce1d2fc719155fa90f
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2023-09-16 17:24:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const db = require("./config/db.js");
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json({limit:'30mb', extended: true}));
+app.use(express.urlencoded({limit:'30mb', extended: true}));
 
 const PORT = process.env.PORT || 5000;
 
